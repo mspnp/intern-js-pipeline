54dc54e4fff4fa85d9b25ab8338ba5bbc1953220
Hannah Zhu <t-hannzhu@microsoft.com>
2022-08-11 09:09:27 -0700

    fixed useState loop

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -10,14 +10,16 @@ export function HomePage() {
     async function fetchRepos() {
       try {
         const asyncResponse = await getRepos('staticwebdev');
-        setRepos(asyncResponse);
+        if([...asyncResponse] != [...repos]){
+          console.log("still changing repos" + asyncResponse + " " + repos)
+          setRepos(asyncResponse);
+        }
       } catch (err) {
         console.error(err);
       }
     }
-    setRepos(['vanilla-basic', 'vanilla-api', 'nextjs-starter', 'nuxtjs-starter', 'angular-basic', 'react-basic', 'vue-basic', 'blazor-starter', 'blazor-basic', 'roles-function', '30DaysOfSWA' ])
-    //fetchRepos();
-  });
+    fetchRepos();
+  }, []);
 
   return (
     <div >
@@ -25,10 +27,10 @@ export function HomePage() {
         <RepoCard repoName='gatsby-test-testharness' orgName= 'HannahZhuSWE'/>
         { repos.map(element => {
 
-          //if(partOfDashboard("staticwebdev",element))
-          //{
+          if(partOfDashboard("staticwebdev",element))
+          {
             return <RepoCard repoName={element} orgName="staticwebdev"/>
-          //}
+          }
 
         })} 
       
